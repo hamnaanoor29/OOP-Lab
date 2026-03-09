fffceba32fe6eccb7dad8dd22da12c73095294d9
hamnanoor29 <hamnaanoor29@gmail.com>
2026-03-09 12:08:00 +0500

    Added Lab Task

--- LAB-TASKS/LAB1/MemberCardDemo.java
+++ LAB-TASKS/LAB1/MemberCardDemo.java
@@ -14,6 +14,7 @@ public class MemberCardDemo
 
         System.out.println(m1);
         System.out.println(m2);
+        System.out.println(m2);
         System.out.println(m3);
         System.out.println(m4);
         System.out.println(m5);
